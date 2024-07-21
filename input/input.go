79c1b6254a9c806692fa18b56fa2be54a72dad0c
Anupam <anupamkmr5200@gmail.com>
2024-07-21 14:26:18 +0530

    [Anupam] added input stream

--- input/input.go
+++ input/input.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+
+	// fmt.Println(`Hello, What is your name?`)
+	// var name string
+	// fmt.Scan(&name)
+	// // stops at space.
+	// fmt.Printf(`Hello, %s!`, name)
+
+	// for multi word input we need to use bufio-> buffered input/output.
+	reader := bufio.NewReader(os.Stdin) // standin is input. i.e keyboard input.
+	fmt.Print("What is your name?\n")   // Print can also be used.
+	fullname, _ := reader.ReadString('\n')
+	fmt.Printf(`Hello, %s`, fullname)
 
 }
